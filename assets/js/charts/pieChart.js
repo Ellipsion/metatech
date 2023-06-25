da30ea68efed8a4d38b7ab1b1f30b345f25e0a38
Ashish Kumawat <ashishskyblue@gmail.com>
2023-06-25 23:36:09 +0530

    changed theme to cyan

--- assets/js/charts/pieChart.js
+++ assets/js/charts/pieChart.js
@@ -11,13 +11,21 @@ const data = {
     {
       label: "Token Distribution",
       data: [1, 2, 2, 15, 25, 55],
-      backgroundColor: (blue_to_pink_gradient = [
-        "rgb(255, 20, 147)", // Deep Pink
-        "rgb(72, 118, 255)", // Light Blue
-        "rgb(135, 64, 224)", // Lavender Blue
-        "rgb(200, 64, 200)", // Orchid
-        "rgb(255, 105, 180)", // Hot Pink
-        "rgb(162, 13, 255)", // Bright Blue
+      // backgroundColor: (blue_to_pink_gradient = [
+      //   "#3BA4A6", // Deep Pink
+      //   "rgb(72, 118, 255)", // Light Blue
+      //   "rgb(135, 64, 224)", // Lavender Blue
+      //   "rgb(200, 64, 200)", // Orchid
+      //   "rgb(255, 105, 180)", // Hot Pink
+      //   "rgb(162, 13, 255)", // Bright Blue
+      // ]),
+      backgroundColor: (gradient = [
+        "#3232f0",
+        "#386fe3",
+        "#3facc5",
+        "#46e6a7",
+        "#4ce68d",
+        "#3BA4A6",
       ]),
       offset: 2,
       hoverOffset: 5,
